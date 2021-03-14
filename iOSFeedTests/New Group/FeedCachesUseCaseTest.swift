d26f4fede3e39196ebecca636955fcee4d49daa1
Abdul <rahimi.diallo96@gmail.com>
2021-03-14 17:30:41 -0400

    request deletion of cache on save

--- iOSFeedTests/New Group/FeedCachesUseCaseTest.swift
+++ iOSFeedTests/New Group/FeedCachesUseCaseTest.swift
@@ -7,21 +7,56 @@
 //
 
 import XCTest
+import iOSFeed
 
 class FeedStore {
     var deleteCacheFeedLoadCount = 0
+    func deleteCacheFeed() {
+        deleteCacheFeedLoadCount += 1
+    }
 }
 
 class LocalFeedLoad {
-    init(store: FeedStore) { }
+    private let store: FeedStore
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func saveOnCache(_ feeds: [Feed]) {
+        store.deleteCacheFeed()
+    }
 }
 
 class FeedCachesUseCaseTest: XCTestCase {
     
     func test_init_doesNotDeleteCacheUponCreation() {
         let feed = FeedStore()
-        let sut = LocalFeedLoad(store: feed)
+        _ = LocalFeedLoad(store: feed)
         XCTAssertEqual(feed.deleteCacheFeedLoadCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let feed = FeedStore()
+            
+        let sut = LocalFeedLoad(store: feed)
+        let feeds = [anyFeed(), anyFeed()]
+        sut.saveOnCache(feeds)
+        
+        XCTAssertEqual(feed.deleteCacheFeedLoadCount, 1)
+    }
+    
+}
+
+//MARK: - Helpers
+
+extension FeedCachesUseCaseTest {
+    
+    func anyURL() -> URL {
+        URL(string: "http://anyurl.com")!
+    }
+    
+    func anyFeed() -> Feed {
+        Feed(id: UUID(), description: "", location: "", image: anyURL())
     }
     
 }
