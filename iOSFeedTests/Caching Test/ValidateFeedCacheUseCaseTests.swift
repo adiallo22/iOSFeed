1eb278860d31e7bdc37da0082808755b96dd2d76
Abdul <rahimi.diallo96@gmail.com>
2021-04-04 13:36:06 -0400

    initiate validation cache test use case

--- iOSFeedTests/Caching Test/ValidateFeedCacheUseCaseTests.swift
+++ iOSFeedTests/Caching Test/ValidateFeedCacheUseCaseTests.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2021 Abdul Diallo. All rights reserved.
 //
 
-import Foundation
+import XCTest
+import iOSFeed
+
+class ValidateFeedCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let (store, _) = makeSUT()
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+
+}
+
+//MARK: - Helpers
+
+extension ValidateFeedCacheUseCaseTests {
+    
+    func makeSUT(currentDate: @escaping () -> Date = Date.init,
+                 file: StaticString = #file,
+                 line: UInt = #line) -> (store: FeedStoreSpy, sut: LocalFeedLoad) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoad(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (store, sut)
+    }
+    
+}
