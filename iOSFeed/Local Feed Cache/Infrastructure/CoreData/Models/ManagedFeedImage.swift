ab65754d981516e6fcb0e65fa4d13f4b3395e019
Abdul <rahimi.diallo96@gmail.com>
2021-05-05 22:55:54 -0400

    new model selection group

--- iOSFeed/Local Feed Cache/Infrastructure/CoreData/Models/ManagedFeedImage.swift
+++ iOSFeed/Local Feed Cache/Infrastructure/CoreData/Models/ManagedFeedImage.swift
@@ -10,13 +10,15 @@ import CoreData
 import Foundation
 
 @objc(ManagedFeedImage)
-private class ManagedFeedImage: NSManagedObject {
+class ManagedFeedImage: NSManagedObject {
     @NSManaged var id: UUID
     @NSManaged var imageDescription: String?
     @NSManaged var location: String?
     @NSManaged var url: URL
     @NSManaged var cache: ManagedCache
-    
+}
+
+extension ManagedFeedImage {
     static func images(from localFeed: [LocalFeedImage],
                        in context: NSManagedObjectContext) -> NSOrderedSet {
         return NSOrderedSet(array: localFeed.map { local in
@@ -34,26 +36,5 @@ private class ManagedFeedImage: NSManagedObject {
                               description: imageDescription,
                               location: location,
                               image: url)
-    }
-}
-
-@objc(ManagedCache)
-private class ManagedCache: NSManagedObject {
-    @NSManaged var timestamp: Date
-    @NSManaged var feed: NSOrderedSet
-    
-    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
-        let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
-        request.returnsObjectsAsFaults = false
-        return try context.fetch(request).first
-    }
-    
-    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
-        try find(in: context).map(context.delete)
-        return ManagedCache(context: context)
-    }
-    
-    var localFeed: [LocalFeedImage] {
-        return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
     }
 }
