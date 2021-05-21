db353bcfb5fc46602fbbdad768179a20de89f02f
Abdul <rahimi.diallo96@gmail.com>
2021-05-21 14:00:28 -0400

    refractor

--- iOSFeed/Local Feed Cache/FeedStore.swift
+++ iOSFeed/Local Feed Cache/FeedStore.swift
@@ -8,27 +8,25 @@
 
 import Foundation
 
-public typealias RetrievedCachedResult =  Result<CachedResult, Error>
-
 public enum CachedResult {
     case empty
     case found(feed: [LocalFeedImage], timestamp: Date)
 }
+public typealias RetrievedCachedResult =  Result<CachedResult, Error>
 
 public protocol FeedStore {
-    
+        
     typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
-    typealias RetrievalCompletion = (RetrievedCachedResult) -> Void
-    
     ///The completion handler can be invoked in any thread
     ///Client is responsible to dispatch to the appropriate thread if neccessasry
     func deleteCacheFeed(completion: @escaping DeletionCompletion)
     
+    typealias InsertionCompletion = (Error?) -> Void
     ///The completion handler can be invoked in any thread
     ///Client is responsible to dispatch to the appropriate thread if neccessasry
     func insert(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
     
+    typealias RetrievalCompletion = (RetrievedCachedResult) -> Void
     ///The completion handler can be invoked in any thread
     ///Client is responsible to dispatch to the appropriate thread if neccessasry
     func retrieve(completion: @escaping RetrievalCompletion)
