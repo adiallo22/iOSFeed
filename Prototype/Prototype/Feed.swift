e13c48b4879c0414bfdc9c9fe7b662156dd494f6
Abdul <rahimi.diallo96@gmail.com>
2021-05-31 09:18:02 -0400

    remove storyboard and add a new scene to the window and make the main

---
+++ Prototype/Prototype/Feed.swift
@@ -0,0 +1,17 @@
+//
+//  Feed.swift
+//  Prototype
+//
+//  Created by Abdul Diallo on 5/31/21.
+//
+
+import UIKit
+
+class Feed: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidAppear(true)
+        view.backgroundColor = .orange
+    }
+    
+}
