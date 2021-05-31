3acad33854531d08bfc0e663b66d84fe8b7fa738
Abdul <rahimi.diallo96@gmail.com>
2021-05-31 10:03:38 -0400

    configure cell

--- Prototype/Prototype/Feed.swift
+++ Prototype/Prototype/Feed.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 private let reuseIdentifier = "FeedCell"
 
@@ -14,6 +15,9 @@ class Feed: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(FeedCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 580
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.separatorStyle = .none
     }
     
 }
@@ -39,11 +43,70 @@ extension Feed {
 
 class FeedCell: UITableViewCell {
     
+    private let pinImage: UIImageView = {
+        let view = UIImageView(image: #imageLiteral(resourceName: "pin"))
+        view.snp.makeConstraints {
+            $0.height.equalTo(12)
+            $0.width.equalTo(12)
+        }
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+    
+    private let locationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "test1"
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        return label
+    }()
+    
+    private lazy var locationContainer: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [pinImage, locationLabel])
+        stack.spacing = 6
+        stack.axis = .horizontal
+        stack.alignment = .center
+        return stack
+    }()
+    
+    private let feedImage: UIImageView = {
+        let view = UIImageView()
+        view.backgroundColor = .lightGray
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "test1test1test1test1test1test1test1test1test1test1test1"
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    private lazy var mainStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [locationContainer, feedImage, descriptionLabel])
+        stack.spacing = 8
+        stack.axis = .vertical
+        return stack
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        backgroundColor = .red
+        layout()
     }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func layout() {
+        selectionStyle = .none
+        contentView.addSubview(mainStack)
+        mainStack.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(5)
+        }
+    }
     
 }
