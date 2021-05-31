736ba56a5b3d9d1f06a0185f73c9cce3dbdff3f4
Abdul <rahimi.diallo96@gmail.com>
2021-05-31 09:27:59 -0400

    convert FeedVC into tableview class. create feedcell

--- Prototype/Prototype/Feed.swift
+++ Prototype/Prototype/Feed.swift
@@ -7,11 +7,43 @@
 
 import UIKit
 
-class Feed: UIViewController {
+private let reuseIdentifier = "FeedCell"
+
+class Feed: UITableViewController {
     
     override func viewDidLoad() {
-        super.viewDidAppear(true)
-        view.backgroundColor = .orange
+        super.viewDidLoad()
+        tableView.register(FeedCell.self, forCellReuseIdentifier: reuseIdentifier)
     }
+    
+}
+
+extension Feed {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) as? FeedCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+}
+
+class FeedCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
 }
