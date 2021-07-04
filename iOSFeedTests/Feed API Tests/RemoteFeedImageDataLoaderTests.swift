dcc91902ff64315b658ee6b4f004af6f8d019b8d
Abdul <rahimi.diallo96@gmail.com>
2021-07-04 17:49:21 -0400

    loading image twice requests url twice

--- iOSFeedTests/Feed API Tests/RemoteFeedImageDataLoaderTests.swift
+++ iOSFeedTests/Feed API Tests/RemoteFeedImageDataLoaderTests.swift
@@ -22,37 +22,47 @@ class RemoteFeedImageDataLoader {
     
 }
 
- class RemoteFeedImageDataLoaderTests: XCTestCase {
-
-     func test_init_doesNotPerformAnyURLRequest() {
-         let (_, client) = makeSUT()
-
-         XCTAssertTrue(client.requestedURLs.isEmpty)
-     }
+class RemoteFeedImageDataLoaderTests: XCTestCase {
+    
+    func test_init_doesNotPerformAnyURLRequest() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
     
     func test_loadImageDataFromURL_requestsDataFromURL() {
-            let url = URL(string: "https://a-given-url.com")!
-            let (sut, client) = makeSUT(url: url)
-
-            sut.loadImageData(from: url) { _ in }
-
-            XCTAssertEqual(client.requestedURLs, [url])
-        }
-
-     private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
-         let client = HTTPClientSpy()
-         let sut = RemoteFeedImageDataLoader(client: client)
-         trackForMemoryLeaks(sut, file: file, line: line)
-         trackForMemoryLeaks(client, file: file, line: line)
-         return (sut, client)
-     }
-
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_loadImageDataFromURLTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.loadImageData(from: url) { _ in }
+        sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
+    private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        return (sut, client)
+    }
+    
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
         
         func get(from url: URL, completion: @escaping (HTTPResponse) -> Void) {
             requestedURLs.append(url)
         }
-     }
+    }
     
- }
+}
