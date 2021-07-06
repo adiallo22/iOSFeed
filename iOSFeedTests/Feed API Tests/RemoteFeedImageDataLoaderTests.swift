41f1278096c031b4df5bc783aab4ca4b9ce53724
Abdul <rahimi.diallo96@gmail.com>
2021-07-05 21:24:36 -0400

    move httpclientspy to a shared scope and remove duplicated test code

--- iOSFeedTests/Feed API Tests/RemoteFeedImageDataLoaderTests.swift
+++ iOSFeedTests/Feed API Tests/RemoteFeedImageDataLoaderTests.swift
@@ -211,39 +211,4 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
-    //MARK: - HTTPClientSpy
-    
-    private class HTTPClientSpy: HTTPClient {
-        private var messages = [(url: URL, completion: (HTTPResponse) -> Void)]()
-        private(set) var cancelledURLs = [URL]()
-        
-        var requestedURLs: [URL] { messages.map { $0.url } }
-        
-        private struct Task: HTTPClientTask {
-            var callBack: () -> Void
-            func cancel() { callBack() }
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPResponse) -> Void) -> HTTPClientTask {
-            messages.append((url, completion))
-            return Task { [weak self] in
-                self?.cancelledURLs.append(url)
-            }
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
-            let response = HTTPURLResponse(
-                url: requestedURLs[index],
-                statusCode: code,
-                httpVersion: nil,
-                headerFields: nil
-            )!
-            messages[index].completion(.success((data, response)))
-        }
-    }
-    
 }
--- iOSFeedTests/Feed API Tests/RemoteFeedLoaderTest.swift
+++ iOSFeedTests/Feed API Tests/RemoteFeedLoaderTest.swift
@@ -9,35 +9,6 @@
 import XCTest
 import iOSFeed
 
-class HTTPClientSpy: HTTPClient {
-    
-    private var messages = [(url: URL, completion: (HTTPResponse) -> Void)]()
-    private(set) var cancelledURLs = [URL]()
-    
-    var requestedURLs: [URL] {
-        return messages.map { $0.url }
-    }
-    
-    private struct Task: HTTPClientTask {
-        func cancel() {}
-    }
-    
-    func get(from url: URL, completion: @escaping (HTTPResponse) -> Void) -> HTTPClientTask {
-        messages.append((url, completion))
-        return Task()
-    }
-    func complete(with error: Error, at index: Int = 0) {
-        messages[index].completion(.failure(error))
-    }
-    func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
-        let response = HTTPURLResponse(url: requestedURLs[index],
-                                       statusCode: code,
-                                       httpVersion: nil,
-                                       headerFields: nil)!
-        messages[index].completion(.success((data, response)))
-    }
-}
-
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
