895a3f527182066db7bf55d41a6b730f0c10a14b
Abdul <rahimi.diallo96@gmail.com>
2021-07-05 21:46:38 -0400

    replace switch by use of flatmap

--- iOSFeed/Remote Feed API/RemoteFeedImageDataLoader.swift
+++ iOSFeed/Remote Feed API/RemoteFeedImageDataLoader.swift
@@ -50,16 +50,12 @@ public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
         task.wrapped = client.get(from: url, completion: { [weak self] result in
             guard self != nil else { return }
             
-            switch result {
-            case .success((let data, let response)):
-                if response.statusCode == 200, !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
-                }
-            case .failure: task.complete(with: .failure(Error.connectivity))
-            }
-            
+            task.complete(with: result
+                            .mapError { _ in Error.connectivity }
+                            .flatMap { (data, response) in
+                                let isValidResponse = !data.isEmpty && response.statusCode == 200
+                                return isValidResponse ? .success(data) : .failure(Error.invalidData)
+                            })
         })
         return task
     }
