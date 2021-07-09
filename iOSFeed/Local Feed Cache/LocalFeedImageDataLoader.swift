1b5995430f3957be32707f1cc160ae7237b3cd1f
Abdul <rahimi.diallo96@gmail.com>
2021-07-09 15:37:34 -0400

    move  to production

---
+++ iOSFeed/Local Feed Cache/LocalFeedImageDataLoader.swift
@@ -0,0 +1,56 @@
+//
+//  LocalFeedImageDataLoader.swift
+//  iOSFeed
+//
+//  Created by Abdul Diallo on 7/9/21.
+//  Copyright © 2021 Abdul Diallo. All rights reserved.
+//
+
+import Foundation
+
+public typealias ImageDataLoaderRESULT = Result<Data, Swift.Error>
+
+public final class LocalFeedImageDataLoader: FeedImageDataLoader {
+    private let store: FeedImageDataStore
+    
+    private final class Task: FeedImageDataLoaderTask {
+        private var completion: ((ImageDataLoaderRESULT) -> Void)?
+
+            init(_ completion: @escaping (ImageDataLoaderRESULT) -> Void) {
+                self.completion = completion
+            }
+
+            func complete(with result: ImageDataLoaderRESULT) {
+                completion?(result)
+            }
+
+            func cancel() {
+                preventFurtherCompletions()
+            }
+
+            private func preventFurtherCompletions() {
+                completion = nil
+            }
+        }
+    
+    public enum Error: Swift.Error {
+        case failed
+        case notFound
+    }
+    
+    public init(store: FeedImageDataStore) {
+        self.store = store
+    }
+    
+    public func loadImage(from url: URL, _ completion: @escaping FeedImageDataLoaderResult) -> FeedImageDataLoaderTask {
+        let task = Task(completion)
+        store.retrieve(dataForURL: url) { [weak self] result in
+            guard self != nil else { return }
+            task.complete(with: result
+                            .mapError { _ in Error.failed }
+                            .flatMap { data in data.map { .success($0) } ?? .failure(Error.notFound) })
+        }
+        return task
+    }
+    
+}
--- iOSFeedTests/Caching Test/LocalFeedImageDataLoaderTests.swift
+++ iOSFeedTests/Caching Test/LocalFeedImageDataLoaderTests.swift
@@ -9,58 +9,6 @@
 import XCTest
 import iOSFeed
 
-private typealias ImageDataLoaderRESULT = Result<Data, Swift.Error>
-
-protocol FeedImageDataStore {
-    typealias Result = Swift.Result<Data?, Error>
-    func retrieve(dataForURL url: URL, completion: @escaping (Result) -> Void)
-}
-
-final class LocalFeedImageDataLoader: FeedImageDataLoader {
-    private let store: FeedImageDataStore
-    
-    private final class Task: FeedImageDataLoaderTask {
-        private var completion: ((ImageDataLoaderRESULT) -> Void)?
-
-            init(_ completion: @escaping (ImageDataLoaderRESULT) -> Void) {
-                self.completion = completion
-            }
-
-            func complete(with result: ImageDataLoaderRESULT) {
-                completion?(result)
-            }
-
-            func cancel() {
-                preventFurtherCompletions()
-            }
-
-            private func preventFurtherCompletions() {
-                completion = nil
-            }
-        }
-    
-    public enum Error: Swift.Error {
-        case failed
-        case notFound
-    }
-    
-    init(store: FeedImageDataStore) {
-        self.store = store
-    }
-    
-    func loadImage(from url: URL, _ completion: @escaping FeedImageDataLoaderResult) -> FeedImageDataLoaderTask {
-        let task = Task(completion)
-        store.retrieve(dataForURL: url) { [weak self] result in
-            guard self != nil else { return }
-            task.complete(with: result
-                            .mapError { _ in Error.failed }
-                            .flatMap { data in data.map { .success($0) } ?? .failure(Error.notFound) })
-        }
-        return task
-    }
-    
-}
-
 class LocalFeedImageDataLoaderTests: XCTestCase {
     
     func test_init_doesNotMessageStoreUponCreation() {
