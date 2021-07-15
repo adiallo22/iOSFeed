d60fbaf9eb2e23dcc09044b3f39d310d8546db35
Abdul <rahimi.diallo96@gmail.com>
2021-07-14 21:09:23 -0400

    move feedimagedatastorespy into its own file

---
+++ iOSFeedTests/Caching Test/Helpers/FeedImageStoreSpy.swift
@@ -0,0 +1,39 @@
+//
+//  FeedImageStoreSpy.swift
+//  iOSFeedTests
+//
+//  Created by Abdul Diallo on 7/14/21.
+//  Copyright © 2021 Abdul Diallo. All rights reserved.
+//
+
+import Foundation
+import iOSFeed
+
+class FeedImageStoreSpy: FeedImageDataStore {
+    
+    enum Message: Equatable {
+        case retrieve(dataFor: URL)
+        case insert(data: Data, for: URL)
+    }
+    
+    private(set) var receivedMessages = [Message]()
+    private var completions = [(FeedImageDataStore.RetrieveResult) -> Void]()
+    
+    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrieveResult) -> Void) {
+        receivedMessages.append(.retrieve(dataFor: url))
+        completions.append(completion)
+    }
+    
+    func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        receivedMessages.append(.insert(data: data, for: url))
+    }
+    
+    func completeRetrieval(with error: Error, at index: Int = 0) {
+        completions[index](.failure(error))
+    }
+    
+    func completeRetrieval(with data: Data?, at index: Int = 0) {
+        completions[index](.success(data))
+    }
+    
+}
