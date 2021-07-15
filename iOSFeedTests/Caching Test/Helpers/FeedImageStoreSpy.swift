bfb84870713a9a46a48a36a03aebc5c79b1a7426
Abdul <rahimi.diallo96@gmail.com>
2021-07-14 21:31:07 -0400

    save and submit error on local image data loader

--- iOSFeedTests/Caching Test/Helpers/FeedImageStoreSpy.swift
+++ iOSFeedTests/Caching Test/Helpers/FeedImageStoreSpy.swift
@@ -19,6 +19,8 @@ class FeedImageStoreSpy: FeedImageDataStore {
     private(set) var receivedMessages = [Message]()
     private var completions = [(FeedImageDataStore.RetrieveResult) -> Void]()
     
+    private var insertionCompletions = [(FeedImageDataStore.InsertionResult) -> Void]()
+    
     func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrieveResult) -> Void) {
         receivedMessages.append(.retrieve(dataFor: url))
         completions.append(completion)
@@ -26,6 +28,7 @@ class FeedImageStoreSpy: FeedImageDataStore {
     
     func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
         receivedMessages.append(.insert(data: data, for: url))
+        insertionCompletions.append(completion)
     }
     
     func completeRetrieval(with error: Error, at index: Int = 0) {
@@ -34,6 +37,10 @@ class FeedImageStoreSpy: FeedImageDataStore {
     
     func completeRetrieval(with data: Data?, at index: Int = 0) {
         completions[index](.success(data))
+    }
+    
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](.failure(error))
     }
     
 }
