967959e485f5720264be98e753d9802c41007cb3
Abdul <rahimi.diallo96@gmail.com>
2021-07-14 21:04:47 -0400

    rename test file

--- iOSFeedTests/Caching Test/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ iOSFeedTests/Caching Test/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 import iOSFeed
 
-class LocalFeedImageDataLoaderTests: XCTestCase {
+class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
     
     func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
