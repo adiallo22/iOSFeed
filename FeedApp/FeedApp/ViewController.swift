356ee9d2f4c6190d73ab9362e1bca380e44719bd
Abdul <rahimi.diallo96@gmail.com>
2021-07-20 22:14:29 -0400

    Add empty feed app project

---
+++ FeedApp/FeedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeedApp
+//
+//  Created by Abdul Diallo on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
