9283a6c9b3bd8cb827790e3f9e8f937cc5776397
Abdul <rahimi.diallo96@gmail.com>
2021-07-25 23:10:02 -0400

    move FeedDataLoaderWithFallbackComposite to production

--- FeedApp/FeedAppTests/FeedDataLoaderWithFallbackCompositeTests.swift
+++ FeedApp/FeedAppTests/FeedDataLoaderWithFallbackCompositeTests.swift
@@ -7,38 +7,7 @@
 
 import XCTest
 import iOSFeed
-
-final class FeedDataLoaderWithFallbackComposite: FeedImageDataLoader {
-    private let primaryDataLoader: FeedImageDataLoader
-    private let fallbackDataLoader: FeedImageDataLoader
-    
-    private class TaskWrapper: FeedImageDataLoaderTask {
-        var wrapped: FeedImageDataLoaderTask?
-        func cancel() {
-            wrapped?.cancel()
-        }
-    }
-    
-    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
-        self.primaryDataLoader = primary
-        self.fallbackDataLoader = fallback
-    }
-    
-    func loadImage(from url: URL, _ completion: @escaping FeedImageDataLoaderResult) -> FeedImageDataLoaderTask {
-        let task = TaskWrapper()
-        task.wrapped = primaryDataLoader.loadImage(from: url) { [weak self] result in
-            switch result {
-            case .success:
-                completion(result)
-                
-            case .failure:
-                task.wrapped = self?.fallbackDataLoader.loadImage(from: url, completion)
-            }
-        }
-        return task
-    }
-    
-}
+import FeedApp
 
 class FeedDataLoaderWithFallbackCompositeTests: XCTestCase {
     
