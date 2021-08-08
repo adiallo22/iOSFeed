40e6e017d756b17be136913723781ac9daf50c68
Abdul <rahimi.diallo96@gmail.com>
2021-08-08 17:29:59 -0400

    add ifdebug compilation directive in order to avoid test code to be added into production

--- FeedApp/FeedApp/SceneDelegate.swift
+++ FeedApp/FeedApp/SceneDelegate.swift
@@ -28,9 +28,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                                 .defaultDirectoryURL()
                                 .appendingPathComponent("FeedStore")
         
+        #if DEBUG
         if CommandLine.arguments.contains("-reset") {
             try? FileManager.default.removeItem(at: localStoreURL)
         }
+        #endif
         
 //        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
 //        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
@@ -55,15 +57,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     private func makeRemoteClient() -> HTTPClient {
-        switch UserDefaults.standard.string(forKey: "connectivity") {
-        case "offline": return AlwaysFailingHTTPClient()
-            
-        default: return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        #if DEBUG
+        if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
+            return AlwaysFailingHTTPClient()
         }
+        #endif
+        return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }
 
 }
 
+#if DEBUG
 private final class AlwaysFailingHTTPClient: HTTPClient {
     private class Task: HTTPClientTask {
         func cancel() { }
@@ -74,4 +78,4 @@ private final class AlwaysFailingHTTPClient: HTTPClient {
         return Task()
     }
 }
-
+#endif
